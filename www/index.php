30ebc24b7cc1bb988e45eb0880b24e245d01eb89
gianfilippa <gianfilippa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-11-24 14:04:54 +0000

    new release 2.3
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phenopix@31 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -44,11 +44,13 @@ echo $contents; } ?>
 <!-- end of project description -->
 <p> 
 
-Package vignettes are currently up to date and available from within R by the usual vignette() command. A copy of them will also be available for download in Research Gate (https://www.researchgate.net/profile/Gianluca_Filippa) where there will be room for comments and discussion. 
+Package vignettes are currently up to date and available from within R by the usual vignette() command. A copy of them will also be available for download on Research Gate (https://www.researchgate.net/profile/Gianluca_Filippa) where there will be room for comments and discussion. 
 
-New version 2.2 features the following news: 
-1. extractVIs is now running in parallel!
-2. New functions available for the processing of coupled RGB and RGB+NIR images to obtain the camera NDVI: matchExposure() and NDVI() 
+New version 2.3 features the following news: 
+1. NDVI function is improved and implemented also for the spatial analysis
+2. All double logistic fittings are improved, so that now data are normalized prior to the fit, making the whole process more stable
+3. A new estimation of uncertainty is proposed, which combines the uncertainties of the various fits and phenophase methods. New functions include combineUncertainty(), summarizePhases(), plotSum(). 
+4. A project page dedicated to the package will soon be published on Research Gate 
 
  </p>
 
